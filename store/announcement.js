454218843d58a4512aef82979c43a40c5d8c8bb1
stan_bk <2568143457@qq.com>
2022-02-03 13:20:49 +0800

    fix: 解决多次发出相同请求的场景

--- store/announcement.js
+++ store/announcement.js
@@ -2,7 +2,8 @@ const state = () => {
   return {
     count: 0,
     noAnnouncement: false,
-    announcements: []
+    announcements: [],
+    isRequest: false // 判断当前存在请求变量--防止用户快速切换路由引起的多次相同请求
   }
 }
 
@@ -12,27 +13,38 @@ const mutations = {
   },
   SET_ANNOUNCEMENTS: (state, content) => {
     state.announcements = !content.isReset ? state.announcements.concat(content.announcements) : []
+  },
+  SET_ISREQUEST_true: (state) => {
+    state.isRequest = true
+  },
+  SET_ISREQUEST_false: (state) => {
+    state.isRequest = false
   }
 }
 
 const actions = {
   getAnnouncements({ state, commit }) {
     return new Promise((resolve, reject) => {
-      this.$axios.$get(`/api/announcement?count=${state.count}`).then(res => {
-        const announcements = res.data
+      if (!state.isRequest) {
+        commit('SET_ISREQUEST_true') // 当前正处于请求状态
+        this.$axios.$get(`/api/announcement?count=${state.count}`).then(res => {
+          commit('SET_ISREQUEST_false') // 请求完毕，现处于未有请求的状态
+          const announcements = res.data
 
-        if (!announcements.length) {
-          commit('SET_noMore_true', undefined, {
-            root: true
-          })
-        } else {
-          commit('SET_COUNT', state.count + announcements.length)
-          commit('SET_ANNOUNCEMENTS', { announcements })
-        }
-        resolve(announcements.length)
-      }).catch(error => {
-        reject(error)
-      })
+          if (!announcements.length) {
+            commit('SET_noMore_true', undefined, {
+              root: true
+            })
+          } else {
+            commit('SET_COUNT', state.count + announcements.length)
+            commit('SET_ANNOUNCEMENTS', { announcements })
+          }
+          resolve(announcements.length)
+        }).catch(error => {
+          commit('SET_ISREQUEST_false') // 请求完毕，现处于未有请求的状态
+          reject(error)
+        })
+      }
     })
   }
 }
--- store/article.js
+++ store/article.js
@@ -2,7 +2,8 @@ const state = () => {
   return {
     count: 0,
     noArticle: false,
-    articles: []
+    articles: [],
+    isRequest: false  // 判断当前存在请求变量--防止用户快速切换路由引起的多次相同请求
   }
 }
 
@@ -12,27 +13,38 @@ const mutations = {
   },
   SET_ARTICLES: (state, content) => {
     state.articles = !content.isReset ? state.articles.concat(content.articles) : []
+  },
+  SET_ISREQUEST_true: (state) => {
+    state.isRequest = true
+  },
+  SET_ISREQUEST_false: (state) => {
+    state.isRequest = false
   }
 }
 
 const actions = {
   getArticles({ state, commit }) {
     return new Promise((resolve, reject) => {
-      this.$axios.$get(`/api/article?count=${state.count}`).then(res => {
-        const articles = res.data
-
-        if (!articles.length) {
-          commit('SET_noMore_true', undefined, {
-            root: true
-          })
-        } else {
-          commit('SET_COUNT', state.count + articles.length)
-          commit('SET_ARTICLES', { articles })
-        }
-        resolve(articles.length)
-      }).catch(error => {
-        reject(error)
-      })
+      if (!state.isRequest) {
+        commit('SET_ISREQUEST_true') // 当前正处于请求状态
+        this.$axios.$get(`/api/article?count=${state.count}`).then(res => {
+          commit('SET_ISREQUEST_false') // 请求完毕，现处于未有请求的状态
+          const articles = res.data
+  
+          if (!articles.length) {
+            commit('SET_noMore_true', undefined, {
+              root: true
+            })
+          } else {
+            commit('SET_COUNT', state.count + articles.length)
+            commit('SET_ARTICLES', { articles })
+          }
+          resolve(articles.length)
+        }).catch(error => {
+          commit('SET_ISREQUEST_false') // 请求完毕，现处于未有请求的状态
+          reject(error)
+        })
+      }
     })
   }
 }
