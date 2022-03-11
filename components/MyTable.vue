4d348691087a7fe7f6fd5473a871f66974ef035f
stan_bk <2568143457@qq.com>
2022-03-11 20:18:33 +0800

    feat: 实现表格组件

---
+++ components/MyTable.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="table">
+    <table class="tb" :class="$store.state.isDarkMode ? 'dark-table' : ''">
+      <thead>
+        <tr>
+          <th v-for="item of prop" :key="item">
+            {{ item }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) of tableData" :key="index">
+          <td v-for="(value, key) in item" :key="key">{{ value }}</td>
+          <slot v-bind="item"></slot>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    data: {
+      type: Array,
+      default() {
+        return []
+      }
+    },
+    prop: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  computed: {
+    tableData() {
+      return this.data.map(item => {
+        const obj = {}
+        this.prop.forEach(key => {
+          if (Object.prototype.hasOwnProperty.call(item, key)) {
+            obj[key] = item[key]
+          } else {
+            obj[key] = undefined
+          }
+        })
+        return obj
+      })
+    }
+  }
+}
+</script>
+<style scoped>
+.table {
+  padding: 20px;
+  border: 1px solid #eee;
+  border-radius: 2px;
+}
+.tb {
+  width: 100%;
+  border-collapse: collapse;
+}
+.dark-table{
+  color: #fff;
+}
+th{
+  padding: 10px 20px;
+}
+thead tr {
+  border-bottom: 1px solid #bbb;
+}
+tr {
+  border-bottom: 1px solid #eee;
+}
+td {
+  width: auto;
+  padding: 20px 20px;
+  text-align: center;
+}
+</style>
--- pages/admin/source.vue
+++ pages/admin/source.vue
@@ -1,5 +1,34 @@
 <template>
   <div>
-    source
+    <my-table :data="info" :prop="['age', 'name', ]">
+      <template #default="slotscope">
+        <button @click="con(slotscope)"></button>
+      </template>
+    </my-table>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      info: [{
+        name: 'wbk',
+        age: 21
+      }, {
+        name: 'cxk',
+        age: 22
+      }]
+    }
+  },
+  mounted() {
+    this.$axios.$get('/api/article').then(res => {
+      this.info = res
+    })
+  },
+  methods: {
+    con(data) {
+      console.log(data)
+    }
+  }
+}
+</script>
