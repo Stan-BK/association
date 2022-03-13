586e378c5b0b1bd0e24a7896051baa9e90df8b2c
stan_bk <2568143457@qq.com>
2022-03-13 15:05:00 +0800

    feat: 实现按钮组组件

---
+++ components/MyButtonGroup.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="buttongroup">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+
+  }
+}
+</script>
+<style scoped>
+.buttongroup {
+  display: inline-block;
+  box-shadow: 1px 1px 1px #eee;
+}
+button {
+  margin: 0;
+  margin-right: -5px;
+  border-radius: 0;
+  border-left: none;
+  border-right: none;
+  box-shadow: none;
+}
+button:first-child {
+  border-top-left-radius: 2px;
+  border-bottom-left-radius: 2px;
+  border-left: 1px solid #eee;
+}
+button:last-child {
+  border-top-right-radius: 2px;
+  border-bottom-right-radius: 2px;
+  border-right: 1px solid #eee;
+}
+</style>
